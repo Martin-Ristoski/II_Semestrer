e731d3de4188f7d90e2b90543f42140059a60518
Martin Ristoski <102248961+Martin-Ristoski@users.noreply.github.com>
2022-03-23 21:10:22 +0100

    Update main.c

--- Lab1_2/main.c
+++ Lab1_2/main.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Glavna poenta na zadachata e da se najde kako se sogleduva dali dve otsechki se sechat, shto ne e moja cel na uchenje.
-//Ostanatoto e ednostavna rabota so strukturi, koja e izrabotena.
 /*
 Да се напише структура која ќе опишува отсечка во дводимензионален простор (две точки).
 Потоа да се напише функција која ќе проверува дали две отсечки се сечат.
 For example:
 
-Input	Result
--2 -2   2 0
- 0  1   1 3
- 0
-
+Input	       Result
+-2 -2  2  0    0
+ 0  1  1  3
 */
-typedef struct tocka{
-int x;
-int y;
+typedef struct tocka
+{
+    float x, y;
 }tocka;
 
-typedef struct otsecka{
-tocka t1;
-tocka t2;
+typedef struct otsecka
+{
+    tocka t1;
+    tocka t2;
 }otsecka;
 
-int se_secat(){
-if(se_secat) return 1;
-else return 0;
-return 0;
+int se_secat (otsecka o1, otsecka o2)
+{
+    float pravec1 = (o1.t1.y-o1.t2.y)/(o1.t1.x-o1.t2.x);
+    float pravec2= (o2.t1.y-o2.t1.y)/(o2.t1.x-o2.t2.x);
+    if(pravec1==pravec2)
+        return 0;
+    else if(o2.t1.x>o1.t1.x && o2.t2.x>o1.t1.x && o2.t2.x>o1.t1.x && o2.t2.x>o1.t2.x)
+        return 0;
+    else if(o1.t1.x>o2.t1.x && o1.t2.x>o2.t1.x && o1.t2.x>o2.t1.x && o1.t2.x>o2.t2.x)
+        return 0;
+    else if(o2.t1.y>o1.t1.y && o2.t2.y>o1.t1.y && o2.t2.y>o1.t1.y && o2.t2.y>o1.t2.y)
+        return 0;
+    else if(o1.t1.y>o2.t1.y && o1.t2.y>o2.t1.y && o1.t2.y>o2.t1.y && o1.t2.y>o2.t2.y)
+        return 0;
+    else
+        return 1;
 }
 
 int main()
 {
-    double x1, y1, x2, y2;
+    float x1, y1, x2, y2;
     scanf("%f %f %f %f", &x1, &y1, &x2, &y2);
     tocka t1 = { x1, y1 };
     tocka t2 = { x2, y2 };
